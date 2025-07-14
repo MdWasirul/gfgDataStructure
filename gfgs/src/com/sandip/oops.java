91da709e38ab254b5f01c99f8d43dbea0f5055ba
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-07-14 22:36:03 +0530

    oops example

--- gfgs/src/com/sandip/oops.java
+++ gfgs/src/com/sandip/oops.java
@@ -3,8 +3,28 @@ package com.sandip;
 public class oops {
 
     public static void main(String[] args) {
-
+//Creating object
+        Student s1=new Student("Basheerul",21);
+        //Calling method:
+        s1.display();
     }
 
 }
 
+//Class definition:
+class Student {
+    //data members
+    private String name;
+    private int age;
+
+    //constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+    public  void display(){
+        System.out.println("Name:"+ name);
+        System.out.println("Age:"+age);
+    }
+}
