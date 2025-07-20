cad132081c03b0d8224a297889689652964a1187
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-07-20 21:16:04 +0530

    String palindrome

---
+++ gfgs/src/com/sandip/String/Palindrome.java
@@ -0,0 +1,32 @@
+package com.sandip.String;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "ABCBA";
+        palindromeString(str);
+        System.out.println(str);
+        ;
+        System.out.println(isPalindrome(str));
+        System.out.println(isPalindrome("abccba"));
+        System.out.println(isPalindrome("abcdba"));
+    }
+
+    static boolean palindromeString(String str) {
+        StringBuilder rev = new StringBuilder(str);
+        rev.reverse();
+        return str.equals(rev.toString());
+
+    }
+
+    static boolean isPalindrome(String str) {
+        int begin = 0;
+        int end = str.length() - 1;
+        while (begin < end) {
+            if (str.charAt(begin) != str.charAt(end))
+                return false;
+            begin++;
+            end--;
+        }
+        return true;
+    }
+}
---
+++ gfgs/src/com/sandip/String/Test.java
@@ -0,0 +1,18 @@
+package com.sandip.String;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "geeks";
+        System.out.println(str.length());
+        System.out.println(str.charAt(3));
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(2, 5));
+    }
+}
+/*
+contain()
+equalTo()
+CompareTo()
+indexOf()
+
+ */
