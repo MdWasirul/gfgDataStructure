800139e8d26bd6a0e7eef310f686253da2faea3d
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-08-20 20:42:05 +0530

    Linked list

--- gfgs/src/com/sandip/LinkLists/intersect.java
+++ gfgs/src/com/sandip/LinkLists/intersect.java
@@ -1,2 +1,59 @@
-package com.sandip.LinkLists;public class intersect {
+package com.sandip.LinkLists;
+
+public class intersect {
+
+    public static class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        int lengthA = 0;
+        int lengthB = 0;
+        while (tempA != null) {
+            lengthA++;
+            tempA = tempA.next;
+
+        }
+        while (tempB != null) {
+            lengthB++;
+            tempB = tempB.next;
+
+        }
+        tempA = headA;
+        tempB = headB;
+        if (lengthB > lengthA) {
+            int steps = lengthB - lengthA;
+            for (int i = 1; i <= steps; i++) {
+                tempB = tempB.next;
+
+            }
+        } else {
+            int steps = lengthA - lengthB;
+            for (int i = 1; i <= steps; i++) {
+                tempA = tempA.next;
+
+            }
+        }
+
+        while (tempA != tempB) {
+            tempA = tempA.next;
+            tempB = tempB.next;
+
+
+        }
+        return tempA;
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 }
--- gfgs/src/com/sandip/LinkLists/nthNode.java
+++ gfgs/src/com/sandip/LinkLists/nthNode.java
@@ -1,6 +1,6 @@
 package com.sandip.LinkLists;
 
-public class nthNodefromEnd {
+public class nthNode {
 
     public static class Node {
         int data;
@@ -10,7 +10,65 @@ public class nthNodefromEnd {
             this.data = data;
         }
     }
-    public static 
+
+    public static Node nthNodeFromEnd(Node head, int pos) {
+        Node temp = head;
+        int size = 0;
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+        temp = head;
+        int mth = size - pos + 1;
+        for (int i = 1; i <= mth - 1; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public static Node nthNodeEnd(Node head, int pos) {
+        Node slow = head;
+        Node fast = head;
+        for (int i = 1; i <= pos; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    public static Node deleteNthNode(Node head, int n) {
+        Node slow = head;
+        Node fast = head;
+        for (int i = 1; i <= n; i++) {
+            fast = fast.next;
+
+        }
+        if (fast == null) {
+            head = head.next;
+            return head;
+
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+
+    }
+
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
 
     public static void main(String[] args) {
         Node a = new Node(100);
@@ -25,6 +83,16 @@ public class nthNodefromEnd {
         c.next = d;
         d.next = e;
         e.next = f;
+//        Node temp = nthNodeFromEnd(a, 2);
+//        System.out.println(temp.data);
+
+//        Node temp = nthNodeEnd(a, 2);
+//        System.out.println(temp.data);
+
+        display(a);
+        a=deleteNthNode(a, 7);
+        System.out.println();
+        display(a);
 
     }
 }
